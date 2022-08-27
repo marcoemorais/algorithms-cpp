bd118f3332136688923cff2e8324ef2011d66acf
Marco Morais <marcoemorais@yahoo.com>
2022-08-27 09:09:06 -0700

    Add method to print iterator ranges

--- include/algorithms/print.h
+++ include/algorithms/print.h
@@ -8,6 +8,13 @@
 #include <utility>
 #include <vector>
 
+template <typename FwdIter>
+struct IterPair
+{
+    FwdIter first;
+    FwdIter last;
+};
+
 namespace std // doctest requires operator<< to be in std:: namespace.
 {
 
@@ -48,6 +55,17 @@ std::ostream&
 operator<<(std::ostream& os, const std::pair<T1, T2>& p)
 {
     os << "{" << p.first << "," << p.second << "}";
+    return os;
+}
+
+template <typename FwdIter,
+          typename T = typename std::iterator_traits<FwdIter>::value_type>
+std::ostream&
+operator<<(std::ostream& os, const IterPair<FwdIter>& p)
+{
+    os << "{";
+    std::copy(p.first, p.last, std::ostream_iterator<T>(os, ","));
+    os << "}";
     return os;
 }
 
